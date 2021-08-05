afe84fb45848ee9a1e366eb68636c70ea5546485
wj <wj@wjui-MacBookPro.local>
2021-08-05 19:31:20 +0900

    script all words

--- noun_crawler.py
+++ noun_crawler.py
@@ -7,19 +7,27 @@ API_URL = "http://localhost:2394/api/noun"
 
 def startCrawling():
     URL = TARGET_DOMAIN + "/wiki/%EB%B6%84%EB%A5%98:%ED%95%9C%EA%B5%AD%EC%96%B4_%EB%AA%85%EC%82%AC"
+    start(URL)
 
-    loadCrawlingPage(URL)
 
-
-def loadCrawlingPage(url):
+def start(url):
     
-    print("loadCrawlingPage:" + url)
+    html = requests.get(url).text
+    soup = BeautifulSoup(html, 'html.parser')
+
+    category = soup.find_all("a", class_="external text")
+
+    loadCrawlerPage(category[0].get('href'))
+    #다음으로 넘어가기 하면 ㄴ, ㄷ, ㄹ 으로 계속 넘어간다. 
+    # for temp in category:
+    #     print(temp)
+    #     loadCrawlerPage(temp.get('href'))
+
+def loadCrawlerPage(url): 
 
     html = requests.get(url).text
     soup = BeautifulSoup(html, 'html.parser')
 
-
-    category = soup.find_all("a", class_="external text")
 
     word_section = soup.find("div", id="mw-pages")
     word_group = word_section.find("div", class_="mw-category-group")
@@ -33,7 +41,7 @@ def loadCrawlingPage(url):
 
     for temp in next_page_group: 
         if(temp.text == '다음 페이지'):
-            loadCrawlingPage(TARGET_DOMAIN+temp.get('href'))
+            loadCrawlerPage(TARGET_DOMAIN+temp.get('href'))
             break
 
 
